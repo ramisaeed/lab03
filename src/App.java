9dd1f1959a4ab9787eaaace2c45e65d660938d65
ramisaeed <rami_sieed@hotmail.com>
2022-07-27 22:01:03 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+        //// 1
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+
+        //2
+
+        int i  = 0;
+
+        for ( i = 0; i < 9; i++)
+        {
+            System.out.println("Hello, World!");
+        }
+
+        for ( i = 9; i > 0; i--)
+        {
+            System.out.println("Hello, World!");
+        }
+
+
+                int x = 0;
+        while (x < 9) {
+            System.out.println("Hello, World!");
+        x++;
+        }
+
+         x = 9;
+        while (x >0 ) {
+            System.out.println("Hello, World!");
+        x--;
+        }
+
+
+///  3 
+
+Random r = new Random();
+
+
+int num = r.nextInt(100) ;
+
+for ( i =  0  ; i < num  ;i++ )
+{
+
+ System.out.println(i);
+ i  = i + 1 ;
+}
+
+
+    
+    }
+}
